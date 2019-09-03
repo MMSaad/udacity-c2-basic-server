506c904270850087bc7279a5e658bbfc5129be63
Mustafa Saad <eng.mustafa.muhammad@gmail.com>
2019-09-04 01:47:07 +0300

    Implement TODOs

--- src/server.ts
+++ src/server.ts
@@ -70,13 +70,53 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get('/cars', (req: Request, res: Response)=>{
+    const {filter} = req.query;
+
+    if( !filter){
+      return res.status(200)
+              .send(cars);
+    }
+    return res.status(200)
+            .send(cars.filter(a=> a.model == filter));
+  });
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get('/car/:id?',(req:Request,res:Response)=>{
+    let {id} = req.params;
+
+    if(!id){
+        return  res.status(400)
+      .send('Id is required');
+    }
+
+      let car = cars.filter(a=> a.id == id);
+      if(car.length == 0){
+        return res.status(400)
+        .send('invalid id');
+      }
+
+      return res.status(200).send(car[0]);
+
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post('/cars',
+    async (req:Request,res:Response)=>{
+      let {id, type,model, cost} = req.body;
+
+      if(!id || !type || !model || !cost){
+        return res.status(400)
+                .send('all fields (Id,Type,Model and Cost are required');
+      }
+
+      cars.push({ make: 'tesla', type: type, model: model, cost: cost, id: id });
+      return res.status(200)
+              .send('Cars updated successfuly');
+  });
 
   // Start the Server
   app.listen( port, () => {
